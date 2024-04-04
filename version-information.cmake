bd13f58a87bb93b1053e6f708b75b451999e6afd
Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
2024-04-04 18:57:53 +0200

    Generate version information header and textfile
    This generates a C++ header file containing version information that can be included
    Additionally a textfile is generated that contains this version information as well
    
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

---
+++ version-information.cmake
@@ -0,0 +1,18 @@
+
+set(EVEREST_VERSION_INFORMATION_HEADER_IN "${CMAKE_CURRENT_LIST_DIR}/assets/version_information.hpp.in")
+set(EVEREST_VERSION_INFORMATION_TXT_IN "${CMAKE_CURRENT_LIST_DIR}/assets/version_information.txt.in")
+
+function (evc_generate_version_information)
+    execute_process(
+        COMMAND
+            git describe --dirty --always --tags
+        WORKING_DIRECTORY
+            ${PROJECT_SOURCE_DIR}
+        OUTPUT_VARIABLE
+            GIT_VERSION
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    # make version information available as c++ header and txt file
+    configure_file("${EVEREST_VERSION_INFORMATION_HEADER_IN}" "${CMAKE_CURRENT_BINARY_DIR}/generated/include/generated/version_information.hpp" @ONLY)
+    configure_file("${EVEREST_VERSION_INFORMATION_TXT_IN}" "${CMAKE_CURRENT_BINARY_DIR}/generated/version_information.txt" @ONLY)
+endfunction()
