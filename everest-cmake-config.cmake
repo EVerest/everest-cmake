23ff30030ad87fee72a08aa3ea46adca5679c17b
Andreas Heinrich <andreas.heinrich@rwth-aachen.de>
2025-04-11 14:20:43 +0200

    Add Protobuf/gRPC helpers to everst-cmake
    * Add function `generate_cpp_from_proto` to generate cpp files
    * Add function `generate_py_from_proto` to generate py files
    * Add setup function for gRPC-generator
    * Add setup function for gRPC
    
    Signed-off-by: Andreas Heinrich <andreas.heinrich@rwth-aachen.de>

--- everest-cmake-config.cmake
+++ everest-cmake-config.cmake
@@ -30,3 +30,8 @@ macro (evc_include FILE)
 endmacro()
 
 include("${CMAKE_CURRENT_LIST_DIR}/version-information.cmake")
+
+include("${CMAKE_CURRENT_LIST_DIR}/protobuf-helpers/setup-grpc.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/protobuf-helpers/setup-generator.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/protobuf-helpers/generate-cpp.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/protobuf-helpers/generate-python.cmake")
