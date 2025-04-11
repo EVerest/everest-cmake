5558bf55700676eae7e6765d77119b0278242241
Andreas Heinrich <andreas.heinrich@rwth-aachen.de>
2025-04-11 19:13:03 +0200

    Add setup.go.cmake file to everest-cmake-config.cmake
    Signed-off-by: Andreas Heinrich <andreas.heinrich@rwth-aachen.de>

--- everest-cmake-config.cmake
+++ everest-cmake-config.cmake
@@ -32,3 +32,4 @@ endmacro()
 include("${CMAKE_CURRENT_LIST_DIR}/version-information.cmake")
 
 include("${CMAKE_CURRENT_LIST_DIR}/golang-support/add-go-target.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/golang-support/setup-go.cmake")
