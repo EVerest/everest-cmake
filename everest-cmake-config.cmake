a06770117420d481e87938f7641dce4703f45b07
Martin Litre <mnlitre@gmail.com>
2026-02-03 14:35:12 +0100

    fix(grpc): Move grpc setup so that cmake works correctly
    Signed-off-by: Martin Litre <mnlitre@gmail.com>

--- everest-cmake-config.cmake
+++ everest-cmake-config.cmake
@@ -21,13 +21,13 @@ if (everest_cmake_FOUND)
 	return()
 endif()
 
-include("${CMAKE_CURRENT_LIST_DIR}/edm.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/python-package-helpers.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/python-virtualenv.cmake")
-
 macro (evc_include FILE)
 	include("${everest-cmake_DIR}/3rd_party/${FILE}.cmake")
 endmacro()
+
+include("${CMAKE_CURRENT_LIST_DIR}/edm.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/python-package-helpers.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/python-virtualenv.cmake")
 
 include("${CMAKE_CURRENT_LIST_DIR}/version-information.cmake")
 
